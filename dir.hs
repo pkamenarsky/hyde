51e2129e904f8231d4915411c4bc49345280e6db
Philip Kamenarsky <pk@harmonious.at>
2012-05-30 02:15:39 +0200

    fs watcher

--- dir.hs
+++ dir.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Directory
 import System.Time
 import Data.List
+import Control.Monad
 
 -- TimeFile
 
@@ -23,16 +24,25 @@ difference old new =
 	Difference
 		(map filepath $ new \\ old)
 		(map filepath $ old \\ new)
-		[]
+		(map filepath $ intersectBy (\(TimeFile (n, t)) (TimeFile (n', t')) -> n == n' && t /= t') old new)
+
+-- IO
 
 getTimestamps :: FilePath -> IO [TimeFile]
 getTimestamps path = do
-	files <- getDirectoryContents path
+	let fpath = map ((path ++ "/")++)
+
+	content <- getDirectoryContents path
+	files <- filterM doesFileExist $ fpath content
+	dirs <- filterM doesDirectoryExist $ fpath $ filter (not . isPrefixOf ".") content
+
 	times <- mapM getModificationTime files
-	return $ map TimeFile $ zip files times
+	rtimes <- mapM getTimestamps dirs
+
+	return $ map TimeFile (zip files times) ++ concat rtimes
 
 main = getTimestamps "."
 
 zero = TOD 0 0
 
-prop_oldfiles = difference [TimeFile ("a", zero), TimeFile ("b", zero)] [TimeFile ("a", zero)]
+prop_oldfiles = difference [TimeFile ("a", zero), TimeFile ("b", zero)] [TimeFile ("a", TOD 0 1)]
