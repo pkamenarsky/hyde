895abc9605d0715934a65be92ba90892a2563604
Philip Kamenarsky <pk@harmonious.at>
2012-05-31 01:56:39 +0200

    logo

--- Global/Site.hs
+++ Global/Site.hs
@@ -24,6 +24,7 @@ site :: Tag -> IO ()
 site content = html HTML5 [CSS $ URL "style.css"] $ body </>
 	[
 	-- vGrid 21 30,
+	imgLink (resources <+> "icon.jpg") home ! Id "logo",
 	content ! Id "main",
 	div ! Id "menu" </> (map (\(item, url) -> div ! Class "menuitem" </> [a item $ home <+> url]) $ zip items urls) ++ [span ! Id "stretcher"],
 	div ! Id "line"]
