fce33a7899366d0711ab273a7f8104e90540fcab
Philip Kamenarsky <pk@harmonious.at>
2012-05-31 19:14:01 +0200

    site

--- Global/Site.hs
+++ Global/Site.hs
@@ -12,19 +12,19 @@ resources = up <+> "resources"
 
 -- Menu
 
-items = ["PHILOSOPHY", "PROCESS", "CLIENTS", "TEAM", "CONTACT"]
+items = ["SERVICES", "PROCESS", "CLIENTS", "TEAM", "CONTACT"]
 urls = map (URL . (++ ".html") . (map toLower)) items
 
 -- Content
 
 title :: String -> Tag
-title title = text title ! Id "title"
+title title = divText title ! Id "title"
 
 site :: Tag -> IO ()
 site content = html HTML5 [CSS $ URL "style.css"] $ body </>
 	[
 	-- vGrid 21 30,
-	imgLink (resources <+> "icon.jpg") home ! Id "logo",
+	imgLink (resources <+> "logo.png") home ! Id "logo",
 	content ! Id "main",
 	div ! Id "menu" </> (map (\(item, url) -> div ! Class "menuitem" </> [a item $ home <+> url]) $ zip items urls) ++ [span ! Id "stretcher"],
 	div ! Id "line"]
--- Site/Philosophy.hs
+++ Site/Philosophy.hs
@@ -1,5 +1,6 @@
 module Site.Philosophy where
 
+import System.IO
 import Global.Site
 import Prelude hiding (div)
 import HTML
@@ -7,8 +8,11 @@ import HTML
 icons :: [Tag]
 icons = map (\y -> imgLink (resources <+> "icon.jpg") home ! Class "icon" ! Style [Top (y * 150) Px]) [0..3]
 
-main = site $ div </>
-	[title "This is nice",
-	text "asdasd" ! Id "column2",
-	div ! Id "column1" </>
-		icons]
+main = do
+	copy <- readFile "Global/philosophy.txt"
+
+	site $ div </>
+		[title "This is nice",
+		text copy ! Id "column2",
+		div ! Id "column1" </>
+			icons]
