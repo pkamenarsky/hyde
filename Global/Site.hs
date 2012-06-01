4246ba39af04c1a1c3e1df01b3354375e7121e58
Philip Kamenarsky <pk@harmonious.at>
2012-06-01 18:58:40 +0200

    site

--- Global/Site.hs
+++ Global/Site.hs
@@ -12,25 +12,26 @@ resources = up <+> "resources"
 
 -- Menu
 
-items = ["SERVICES", "PROCESS", "CLIENTS", "TEAM", "CONTACT"]
+items = ["ABOUT US", "DEVELOPMENT", "CLIENTS", "TEAM", "CONTACT"]
 urls = map (URL . (++ ".html") . (map toLower)) items
 
 -- Content
 
 title title = divText title ! Id "title"
 
-menu = table ! Id "menu" </> [tr ! Class "menurow" </>
+menu active = table ! Id "menu" </> [tr ! Class "menurow" </>
 	map (\(item, url, i) ->
-			tdTag (a item $ home <+> url) !
-			Class (if i == 0 then "menuleft" else "menurest"))
-		(zip3 items urls [0..])]
+			tdTag (a item (home <+> url) ! Class (chose i active "menuactive" "menuinactive")) !
+			Class (chose i 0 "menuleft" "menurest"))
+		(zip3 items urls [0..])] where
+			chose a b c d = if a == b then c else d
 
-site :: Tag -> IO ()
-site content = html HTML5 [CSS $ URL "style.css"] $ body </>
+site :: Int -> Tag -> IO ()
+site active content = html HTML5 [CSS $ URL "style.css"] $ body </>
 	[
 	-- vGrid 21 30,
 	imgLink (resources <+> "logo.png") home ! Id "logo",
-	menu,
+	menu active,
 	div ! Id "line",
 	div ! Id "main" </>
 		[content]]
