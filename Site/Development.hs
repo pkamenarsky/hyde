6e5034b4ad56b3c5cd8c4e491ee4392a586a25ec
Philip Kamenarsky <pk@harmonious.at>
2012-06-04 02:20:13 +0200

    tweaks

---
+++ Site/Development.hs
@@ -0,0 +1,21 @@
+module Site.Philosophy where
+
+import System.IO
+import Global.Site
+import Prelude hiding (div)
+import HTML
+
+pitch copy = div ! Class "pitch" </> [divText copy ! Class "pitch-copy"]
+
+titledPitch icon title copy = div ! Class "pitch" </>
+	[
+	-- img (resources <+> icon) ! Class "pitch-icon",
+	divText title ! Class "pitch-title",
+	divText copy ! Class "pitch-copy"]
+
+main = do
+	copy <- readFile "Global/dev1.html"
+
+	site 1 $ div </>
+		[title "It pays to do things right the first time.",
+		pitch copy]
