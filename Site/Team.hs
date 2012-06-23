1848f4e541155da484fa45204fbce84246834e57
Philip Kamenarsky <pk@harmonious.at>
2012-06-23 23:56:52 +0200

    photo

--- Site/Team.hs
+++ Site/Team.hs
@@ -7,4 +7,6 @@ import HTML
 
 main = do
 	site 3 0 $ div </>
-		[title "Our team"]
+		[title "Our team",
+		img (resources <+> "phil.jpg") ! Class "team-logo",
+		divText "<b>Philip Kamenarsky</b> When building a backend sys&shy;tem we aim for stability, main&shy;tain&shy;abili&shy;ty, precise error repor&shy;ting and efficient procedures for iden&shy;tifying and removing system faults.When building a backend sys&shy;tem we aim for stability, main&shy;tain&shy;abili&shy;ty, precise error repor&shy;ting and efficient procedures for iden&shy;tifying and removing system faults." ! Class "team-text"]
